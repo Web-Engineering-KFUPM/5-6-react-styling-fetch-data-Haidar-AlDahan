8cda09cff6aac5b3ab0aca0f8a8cf419e596ddc1
Haidar AlDahan <haidr.r.aldahan@gmail.com>
2026-03-04 09:43:35 +0300

    fixed grader feedback

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -210,7 +210,7 @@ export default function App() {
 
       try {
         const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users",
+          "https://jsonplaceholder.typicode.com/users"
         );
 
         if (!response.ok) {
